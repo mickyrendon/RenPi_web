a57259ade77c0f8fa35a76936a35c8c9fbd420ca
micky rendon <elderendon@hotmail.es>
2024-03-28 18:15:31 -0300

    [add] services section

---
+++ src/components/ServicesCards/servicesContent.js
@@ -0,0 +1,47 @@
+export const sContent = [
+    {
+        title: 'Estrategia digital',
+        content: 'consiste en definir los objetivos, las acciones y las herramientas que se utilizarán para promocionar una marca, un producto o un servicio en Internet. Una estrategia digital debe estar basada en un análisis del mercado, la competencia, el público objetivo y los recursos disponible',
+        url: './assets/moodboard/seo.svg'
+    },
+    {
+        title: 'Posicionamiento web (SEO):',
+        content: 'consiste en optimizar un sitio web para que aparezca en los primeros lugares de los resultados de búsqueda de los motores como Google o Bing. Para ello, se deben mejorar aspectos técnicos, de contenido y de autoridad del sitio web, utilizando palabras clave relevantes para el negocio ',
+        url: './assets/moodboard/seo.svg'
+    },
+    {
+        title: 'Publicidad online (SEM):',
+        content: 'Publicidad online (SEM): consiste en crear y gestionar campañas de anuncios pagados en plataformas como Google Ads o Facebook Ads. Estas campañas permiten llegar a un público específico, segmentado por criterios como la ubicación, el género, la edad, los intereses o el comportamiento ',
+        url: './assets/moodboard/sem.svg'
+    },
+    {
+        title: 'Gestión de redes sociales:',
+        content: 'Gestión de redes sociales: consiste en crear y mantener perfiles en las principales redes sociales como Facebook, Twitter, Instagram o LinkedIn. El objetivo es generar contenido de valor, interactuar con los usuarios, aumentar el alcance y la visibilidad de la marca y fidelizar a los clientes ',
+        url: './assets/moodboard/redes-sociales.svg'
+    },
+    {
+        title: 'Creación de contenidos:',
+        content: 'Creación de contenidos: consiste en producir textos, imágenes, vídeos, infografías u otros formatos que aporten información útil, entretenida o educativa a los usuarios. El contenido debe estar alineado con la estrategia digital, las palabras clave y los objetivos de la marca ',
+        url: './assets/moodboard/creacion-contenido.svg'
+    },
+    {
+        title: 'Diseño web:',
+        content: 'consiste en crear o mejorar el aspecto visual y funcional de un sitio web. El diseño web debe ser atractivo, intuitivo, responsive (adaptado a diferentes dispositivos) y optimizado para el SEO ',
+        url: './assets/moodboard/desarrollo-web.svg'
+    },
+    {
+        title: 'Analítica web:',
+        content: 'Analítica web: consiste en medir y analizar el rendimiento de las acciones digitales, utilizando herramientas como Google Analytics o Facebook Insights. La analítica web permite conocer el tráfico, las conversiones, el comportamiento y la satisfacción de los usuarios, así como identificar oportunidades de mejora ',
+        url: './assets/moodboard/analitica.svg'
+    },
+    {
+        title: 'Fotografía:',
+        content: 'consiste en capturar y editar imágenes profesionales para mostrar el producto o servicio de una forma atractiva y persuasiva. La fotografía puede mejorar la imagen de marca, el posicionamiento web, la publicidad online, el contenido de redes sociales y el diseño web ',
+        url: './assets/moodboard/fotografia.svg'
+    },
+    {
+        title: 'Identidad de marca:',
+        content: 'consiste en crear o mejorar la representación visual y verbal de una marca, que incluye elementos como el logotipo, los colores, la tipografía, el eslogan, el tono y la personalidad ',
+        url: './assets/moodboard/branding.svg'
+    }
+]
