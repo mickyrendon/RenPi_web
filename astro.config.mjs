4ef6912c5fa1e29b16c23672364c286196f07251
micky rendon <elderendon@hotmail.es>
2024-04-24 15:27:40 -0400

    [features] services description cards & seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations:[
-        tailwind({}), 
-        react({include: ['**/react/*'],})
-    ]
+  site: 'https://renpi.cl',
+  integrations: [tailwind({}), react({
+    include: ['**/react/*']
+  }), sitemap()]
 });
