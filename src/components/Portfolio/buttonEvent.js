011120e1ae1601526d156cee4714c54196bed5b4
mickyrendon <mickyrendon42@gmail.com>
2025-01-14 11:51:39 -0300

    [add feature] portfolio logic finished

--- src/components/Portfolio/buttonEvent.js
+++ src/components/Portfolio/buttonEvent.js
@@ -1,35 +1,29 @@
+// callback
+export const toggleGallery = (buttonId) => {
+    // gallery container child nodes / portfolio cards
+    const childNodes = document.getElementById('proyects-gallery').children;
 
-//getting the button id 
-export const toggleGallery = (categoryId) => {
-    //gallery container child nodes / portfolio cards
-    const childNodes = document.getElementById('proyects-gallery').childNodes;
-    let list = [];
-
-    console.log(childNodes.length);
     // iterating over the child nodes
     for (let i = 0; i < childNodes.length; i++) {
         // getting the first child of the child node
         const firstChild = childNodes[i].firstElementChild;
         // getting the parent of the first child
-        const parent = childNodes[i]
+        const parent = childNodes[i];
         
         // if the first child exists
         if (firstChild) {
-            // getting the tag of the first child / card proyect tag
-            let tag = firstChild.innerText;
-            // if the tag is different from the button id
-            if (tag !== categoryId) {
-                // hide the parent / proyect card
-                parent.classList.toggle('hidden');
-            // if the tag is equal to the button id 'todas'
-            }else if (categoryId === 'todas') {
-                // show all the cards
+            // getting the tag of the first child / card project tag
+            const tag = firstChild.innerText.trim()
+           console.log(firstChild, tag);
+            
+            // if the tag is different from the button id and the button id is not 'todas'
+            if (tag !== buttonId && buttonId !== 'todas') {
+                // hide the parent / project card
+                parent.classList.add('hidden');
+            } else {
+                // show the parent / project card
                 parent.classList.remove('hidden');
             }
-            list.push(tag);
         }
     }
-    
-    // return {buttons, title, content, imgAlt, bkgImage, iconImage}
-
 }
